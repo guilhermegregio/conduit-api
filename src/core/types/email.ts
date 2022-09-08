edd76eec851f53e3df7d3076dd4812f4241eeaa0
Rúvila Magalhães <ruvila.magalhaes@creditas.com>
2022-09-08 11:54:17 -0300

    feat: add slug and test

--- src/core/types/email.ts
+++ src/core/types/email.ts
@@ -2,19 +2,24 @@ import * as t from 'io-ts'
 import { withMessage } from 'io-ts-types'
 
 interface EmailBrand {
-  readonly Email: unique symbol // use `unique symbol` here to ensure uniqueness across modules / packages
+  readonly Email: unique symbol;
 }
 
 const isEmail = (value: string) => {
-  return /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)
+  return /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+    value,
+  )
 }
 
-const emailCodec = withMessage(t.brand(
-  t.string,
-  (value): value is t.Branded<string, EmailBrand> => isEmail(value),
-  'Email', // the name must match the readonly field in the brand
-), () => 'Invalid email')
+const emailCodec = withMessage(
+  t.brand(
+    t.string,
+    (value): value is t.Branded<string, EmailBrand> => isEmail(value),
+    'Email',
+  ),
+  () => 'Invalid email',
+)
 
-type Email = t.TypeOf<typeof emailCodec>
+type Email = t.TypeOf<typeof emailCodec>;
 
 export { emailCodec, Email }
--- src/core/types/password.ts
+++ src/core/types/password.ts
@@ -2,7 +2,7 @@ import * as t from 'io-ts'
 import { withMessage } from 'io-ts-types'
 
 interface PasswordBrand {
-  readonly Password: unique symbol; // use `unique symbol` here to ensure uniqueness across modules / packages
+  readonly Password: unique symbol;
 }
 
 const isPassword = (value: string) => {
@@ -13,7 +13,7 @@ const passwordCodec = withMessage(
   t.brand(
     t.string,
     (value): value is t.Branded<string, PasswordBrand> => isPassword(value),
-    'Password', // the name must match the readonly field in the brand
+    'Password',
   ),
   () => 'Invalid Password',
 )
