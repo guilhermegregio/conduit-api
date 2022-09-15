176c4d30ffe3481b9b696d70ca2e16c81cf58ff9
Rúvila Magalhães <ruvila.magalhaes@creditas.com>
2022-09-15 10:50:48 -0300

    refactor: user types tests

--- src/core/user/types.test.ts
+++ src/core/user/types.test.ts
@@ -23,55 +23,57 @@ it('should invalid user', () => {
   expect(E.isLeft(user)).toBeTruthy()
 })
 
-it('should valid create user', () => {
-  const userValid = {
-    username: 'jacob',
-    email: 'jake@jake.jake',
-    password: 'jakejake',
-  }
+describe('create user', () => {
+  it('should validate create user', () => {
+    const userValid = {
+      username: 'jacob',
+      email: 'jake@jake.jake',
+      password: 'jakejake',
+    }
 
-  const assert = (value: any) => expect(value).toEqual(userValid)
+    const assert = (value: any) => expect(value).toEqual(userValid)
 
-  pipe(userValid, createUserCodec.decode, E.map(assert), E.mapLeft(assert))
-})
+    pipe(userValid, createUserCodec.decode, E.map(assert), E.mapLeft(assert))
+  })
 
-it('should invalidade create user with invalid slug', () => {
-  const userValid = {
-    username: '1invalid-user',
-    email: 'jake@jake.jake',
-    password: 'jakejake',
-  }
+  it('should invalidate create user with invalid slug', () => {
+    const userInvalid = {
+      username: '1invalid-user',
+      email: 'jake@jake.jake',
+      password: 'jakejake',
+    }
 
-  const assert = (errors: any) =>
-    expect(errors[0].message).toBe('Invalid slug')
+    const assert = (errors: any) =>
+      expect(errors[0].message).toBe('Invalid slug')
 
-  pipe(userValid, createUserCodec.decode, E.map(assert), E.mapLeft(assert))
-})
+    pipe(userInvalid, createUserCodec.decode, E.map(assert), E.mapLeft(assert))
+  })
 
-it('should invalidade create user with invalid email', () => {
-  const userValid = {
-    username: 'user-valid',
-    email: 'jakejake.jake',
-    password: 'jakejake',
-  }
+  it('should invalidate create user with invalid email', () => {
+    const userInvalid = {
+      username: 'user-valid',
+      email: 'jakejake.jake',
+      password: 'jakejake',
+    }
 
-  const assert = (errors: any) =>
-    expect(errors[0].message).toBe('Invalid email')
+    const assert = (errors: any) =>
+      expect(errors[0].message).toBe('Invalid email')
 
-  pipe(userValid, createUserCodec.decode, E.map(assert), E.mapLeft(assert))
-})
+    pipe(userInvalid, createUserCodec.decode, E.map(assert), E.mapLeft(assert))
+  })
 
-it('should invalidade create user with invalid password', () => {
-  const userValid = {
-    username: 'user-valid',
-    email: 'jake@jake.jake',
-    password: 'ja',
-  }
+  it('should invalidate create user with invalid password', () => {
+    const userInvalid = {
+      username: 'user-valid',
+      email: 'jake@jake.jake',
+      password: 'ja',
+    }
 
-  const assert = (errors: any) =>
-    expect(errors[0].message).toBe('Invalid Password')
+    const assert = (errors: any) =>
+      expect(errors[0].message).toBe('Invalid Password')
 
-  pipe(userValid, createUserCodec.decode, E.map(assert), E.mapLeft(assert))
+    pipe(userInvalid, createUserCodec.decode, E.map(assert), E.mapLeft(assert))
+  })
 })
 
 describe('update user', () => {
@@ -96,10 +98,6 @@ describe('update user', () => {
   it('should invalidate update user with invalid e-mail', () => {
     const updateInvalidUser = {
       email: 'jakejake.jake',
-      bio: 'I like to skateboard',
-      image: 'https://i.stack.imgur.com/xHWG8.jpg',
-      username: 'jacob',
-      password: 'jakejake',
     }
     const assert = (errors: any) =>
       expect(errors[0].message).toBe('Invalid email')
@@ -114,11 +112,7 @@ describe('update user', () => {
 
   it('should invalidate update user with invalid image', () => {
     const updateInvalidUser = {
-      email: 'jake@jake.jake',
-      bio: 'I like to skateboard',
       image: 'htt',
-      username: 'jacob',
-      password: 'jakejake',
     }
     const assert = (errors: any) =>
       expect(errors[0].message).toBe('Invalid URL')
@@ -133,11 +127,7 @@ describe('update user', () => {
 
   it('should invalidate update user with invalid username', () => {
     const updateInvalidUser = {
-      email: 'jake@jake.jake',
-      bio: 'I like to skateboard',
-      image: 'https://i.stack.imgur.com/xHWG8.jpg',
       username: 'ja',
-      password: 'jakejake',
     }
     const assert = (errors: any) =>
       expect(errors[0].message).toBe('Invalid slug')
@@ -152,10 +142,6 @@ describe('update user', () => {
 
   it('should invalidate update user with invalid password', () => {
     const updateInvalidUser = {
-      email: 'jake@jake.jake',
-      bio: 'I like to skateboard',
-      image: 'https://i.stack.imgur.com/xHWG8.jpg',
-      username: 'jacob',
       password: 'ja',
     }
     const assert = (errors: any) =>
