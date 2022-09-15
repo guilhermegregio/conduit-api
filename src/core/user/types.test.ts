f930131400f2bf6b597538a5a201ea482b7ef512
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2022-09-15 09:48:13 -0300

    feat: add create user codec and type

--- src/core/user/types.test.ts
+++ src/core/user/types.test.ts
@@ -1,5 +1,6 @@
-import { isRight, isLeft } from 'fp-ts/Either'
-import { userCodec } from './types'
+import * as E from 'fp-ts/Either'
+import { pipe } from 'fp-ts/function'
+import { createUserCodec, userCodec } from './types'
 
 it('should valid user', () => {
   const user = userCodec.decode({
@@ -9,7 +10,7 @@ it('should valid user', () => {
     bio: '',
   })
 
-  expect(isRight(user)).toBeTruthy()
+  expect(E.isRight(user)).toBeTruthy()
 })
 
 it('should invalid user', () => {
@@ -19,5 +20,73 @@ it('should invalid user', () => {
     username: 1,
   })
 
-  expect(isLeft(user)).toBeTruthy()
+  expect(E.isLeft(user)).toBeTruthy()
+})
+
+it('should valid create user', () => {
+  const userValid = {
+    username: 'jacob',
+    email: 'jake@jake.jake',
+    password: 'jakejake',
+  }
+
+  const assert = (value: any) => expect(value).toEqual(userValid)
+
+  pipe(
+    userValid,
+    createUserCodec.decode,
+    E.map(assert),
+    E.mapLeft(assert),
+  )
+})
+
+it('should invalidade create user with invalid slug', () => {
+  const userValid = {
+    username: '1invalid-user',
+    email: 'jake@jake.jake',
+    password: 'jakejake',
+  }
+
+  const assert = (errors: any) => expect(errors[0].message).toBe('Invalid slug')
+
+  pipe(
+    userValid,
+    createUserCodec.decode,
+    E.map(assert),
+    E.mapLeft(assert),
+  )
+})
+
+it('should invalidade create user with invalid email', () => {
+  const userValid = {
+    username: 'user-valid',
+    email: 'jakejake.jake',
+    password: 'jakejake',
+  }
+
+  const assert = (errors: any) => expect(errors[0].message).toBe('Invalid email')
+
+  pipe(
+    userValid,
+    createUserCodec.decode,
+    E.map(assert),
+    E.mapLeft(assert),
+  )
+})
+
+it('should invalidade create user with invalid password', () => {
+  const userValid = {
+    username: 'user-valid',
+    email: 'jake@jake.jake',
+    password: 'ja',
+  }
+
+  const assert = (errors: any) => expect(errors[0].message).toBe('Invalid Password')
+
+  pipe(
+    userValid,
+    createUserCodec.decode,
+    E.map(assert),
+    E.mapLeft(assert),
+  )
 })
--- src/core/user/types.ts
+++ src/core/user/types.ts
@@ -1,4 +1,7 @@
 import * as t from 'io-ts'
+import { emailCodec } from '../types/email'
+import { passwordCodec } from '../types/password'
+import { slugCodec } from '../types/slug'
 
 const userCodecRequired = t.type({
   email: t.string,
@@ -15,4 +18,12 @@ const userCodec = t.intersection([userCodecRequired, userCodecPartial])
 
 type User = t.TypeOf<typeof userCodec>
 
-export { userCodec, User }
+const createUserCodec = t.type({
+  username: slugCodec,
+  email: emailCodec,
+  password: passwordCodec,
+})
+
+type CreateUser = t.TypeOf<typeof createUserCodec>
+
+export { userCodec, User, createUserCodec, CreateUser }
