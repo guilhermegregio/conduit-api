725ea9f98c60ef190ea8cf0eb5ecc89c87289737
Rúvila Magalhães <ruvila.magalhaes@creditas.com>
2022-09-22 11:33:03 -0300

    feat: add lib jose and improve user response

--- src/ports/express/server.ts
+++ src/ports/express/server.ts
@@ -1,20 +1,22 @@
 import * as TE from 'fp-ts/TaskEither'
 import express from 'express'
-import bodyParser from 'body-parser'
 import { registerUser } from '@/core/user/use-case/register-user'
 import { pipe } from 'fp-ts/lib/function'
 
 const app = express()
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.post('/api/users', (req, res) => {
   return pipe(
     req.body.user,
-    registerUser<string>(async () => 'sucesso'),
-    TE.map(result => res.json(result)),
-    TE.mapLeft(result => res.json(result)),
+    registerUser<any>(async (user) => ({ ...user, id: Date.now() })),
+    TE.map((result) => {
+      const { id, ...response } = result
+      res.json({ user: { ...response, token: 'jwt.token' } })
+    }),
+    TE.mapLeft((result) => res.json(result)),
   )()
 })
 
