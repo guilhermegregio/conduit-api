c432858746e4080783a340cb280d8ec0bc328561
Reginaldo Junior <76regi@gmail.com>
2022-09-22 12:04:21 -0300

    feat: add generate jwt create user

--- src/ports/express/server.ts
+++ src/ports/express/server.ts
@@ -2,19 +2,34 @@ import * as TE from 'fp-ts/TaskEither'
 import express from 'express'
 import { registerUser } from '@/core/user/use-case/register-user'
 import { pipe } from 'fp-ts/lib/function'
+import * as jose from 'jose'
 
 const app = express()
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.post('/api/users', (req, res) => {
+const secret = Buffer.from('asdfasdfasdfasdfasfasdfsafasdfsadfasfsdfasfasdf')
+const generateJWT = async (payload: any) => {
+  return new jose.SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setExpirationTime('2h')
+    .sign(secret)
+}
+
+const verifyJWT = async (token: any) => {
+  return jose.jwtVerify(token, secret)
+}
+
+app.post('/api/users', async (req, res) => {
   return pipe(
     req.body.user,
     registerUser<any>(async (user) => ({ ...user, id: Date.now() })),
-    TE.map((result) => {
+    TE.map(async (result) => {
       const { id, ...response } = result
-      res.json({ user: { ...response, token: 'jwt.token' } })
+      const token = await generateJWT({ id })
+      console.log(await verifyJWT(token))
+      res.json({ user: { ...response, token } })
     }),
     TE.mapLeft((result) => res.json(result)),
   )()
